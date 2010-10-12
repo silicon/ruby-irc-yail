5e90def35f8e15013f7a61c03e1f930797497bc2
silicon <silicon@dhcp-102.lan.hectomertz.com>
2010-10-11 21:54:13 -0700

    adjusted spacing and fixed usage

--- lib/net/yail.rb
+++ lib/net/yail.rb
@@ -606,8 +606,8 @@ class YAIL
 
         # Notices come from server sometimes, so... another merger for legacy fun!
         nick = event.server? ? '' : event.nick
-	from = event.server? ? nick : event.from
-        handle(event.type, event.from, nick, target, event.text)
+        from = event.server? ? nick : event.from
+        handle(event.type, from, nick, target, event.text)
 
       # This is a bit painful for right now - just use some hacks to make it work semi-nicely,
       # but let's not put hacks into the core Event object.  Modes need reworking soon anyway.
